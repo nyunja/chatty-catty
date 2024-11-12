772d1eabe50996a1ad95866b90766fb6348f0286
johnotieno0 <poptiff@gmail.com>
2024-11-13 00:02:45 +0300

    feat: implement LoadConfig() in config.go loads the environment variables, ensuring that environment variable access is centralized

--- config/config.go
+++ config/config.go
@@ -1,2 +1,19 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	APIKey string
+}
+
+func LoadConfig() (*Config, error) {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		return nil, fmt.Errorf("API_KEY environment variable not set")
+	}
+
+	return &Config{APIKey: apiKey}, nil
+}
