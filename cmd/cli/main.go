7fc06993d514498ea6b5b7f1b481d91ef0187b08
johnotieno0 <poptiff@gmail.com>
2024-11-13 23:09:23 +0300

    refactor: call NewChatClient to start a new session

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,21 +6,17 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
-
-	"github.com/google/generative-ai-go/genai"
-	"google.golang.org/api/option"
 )
 
 func main() {
 	fmt.Println("Therapy Cat Chatbot is starting... 🐱‍💻🐱‍💻")
-	cfg, err := config.LoadConfig()
-	if err!= nil {
-        log.Fatalf("Error: %v", err)
-        return
-    }
+	err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+		return
+	}
 	ctx := context.Background()
-	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv(cfg.APIKey)))
+	client, err := lib.NewChatClient(ctx)
 	if err != nil {
 		log.Fatalln("Error: unable to initiate client")
 		return
