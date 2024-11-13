b46aaaa63199433e91b69189c2bde87f4802537a
johnotieno0 <poptiff@gmail.com>
2024-11-13 21:27:50 +0300

    org: move helper fucntions to utils.go

---
+++ lib/utils.go
@@ -0,0 +1,38 @@
+package lib
+
+import (
+	"bufio"
+	"os"
+)
+
+func Cap(s string) string {
+	result := ""
+	if string(s[0]) >= "a" && string(s[0]) <= "z" {
+		result += string(s[0] - 32)
+	}
+	return result + s[1:]
+}
+
+func toLower(s string) string {
+	result := ""
+	for _, ch := range s {
+		if ch >= 'a' && ch <= 'z' {
+			result += string(ch)
+		} else {
+			result += string(ch + 32)
+		}
+	}
+	return result
+}
+
+func printJill() {
+	file, err := os.Open("jill.txt")
+	if err != nil {
+		os.Stdout.WriteString("Error: problem opening file")
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		os.Stdout.WriteString(scanner.Text() + "\n")
+	}
+
+}
