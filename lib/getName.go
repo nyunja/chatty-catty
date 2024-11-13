f7fd3146f6642621e81aa5e304f5a4f319c81703
johnotieno0 <poptiff@gmail.com>
2024-11-13 23:08:11 +0300

    refactor: getNames to use Persona and DefaultIntro from config.go

--- lib/getName.go
+++ lib/getName.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	cfg "chatty/config"
 )
 
 func validateName(name string) (string, error) {
@@ -42,13 +44,7 @@ func getName() (string, string, string) {
 	// sep := checkOps()
 	name2 := Cap(name)
 	os.Stdout.WriteString("\r\n")
-	persona := `Pretend you are a therapist cat who can talk called Jill. 
-	Respond to user input like a therapy cat who can talk. 
-	Only include what you would say in the response.
-	Follow up with a question where necessary. 
-	If the user input is out of your scope, you can ask the user to rephrase.
-	Or tell them to stay within topics that you might be able to assist. 
-	Remember the conversation to keep it flowing. The user is ` + name2
-	intro := "Hi " + name2 + ", how are you feeling today?"
+	persona := fmt.Sprintf(cfg.Persona, name2)
+	intro := fmt.Sprint(cfg.DefaultIntro, name2)
 	return persona, intro, name2
 }
