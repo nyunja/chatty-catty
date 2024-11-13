48b92c78b1d9fbb61196436c22114503a2911a82
johnotieno0 <poptiff@gmail.com>
2024-11-13 22:31:40 +0300

    org: implement NewChatClient to manage API client creation and error handling

---
+++ lib/client.go
@@ -0,0 +1,21 @@
+package lib
+
+import (
+	cfg "chatty/config"
+	"context"
+	"fmt"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func NewChatClient(ctx context.Context) (*genai.Client, error) {
+	if cfg.API_KEY == "" {
+		return nil, cfg.ErrNoAPIKey
+	}
+	client, err := genai.NewClient(ctx, option.WithAPIKey(cfg.API_KEY))
+	if err!= nil {
+		return nil, fmt.Errorf("failed to create client: %v", err)
+	}
+	return client, nil
+}
