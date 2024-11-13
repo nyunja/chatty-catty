2b9aa9822fb2263a17e1892d535cc382f900d0e0
johnotieno0 <poptiff@gmail.com>
2024-11-13 22:25:03 +0300

    org: add a configuration structure to manage static text like the persona or model name

--- config/config.go
+++ config/config.go
@@ -14,8 +14,8 @@ type Config struct {
 var (
 	API_KEY string
 	ModelName string = "gemini-pro"
-	Perosna string = "Pretend you are a therapy cat called Jill. Only respond like a cat therapist."
-	DefaultIntro string = "Hello! I'm Jill, a therapy cat. I'll guide you through your therapy session. Remember, I'm just a helpful assistant, not a replacement for a professional"
+	Persona string = "Pretend you are a therapy cat called Jill. Only respond like a cat therapist. The user is %s"
+	DefaultIntro string = "Hi %s! I'm Jill, a therapy cat. I'll guide you through your therapy session. Remember, I'm just a helpful assistant, not a replacement for a professional"
 	EndCommand string = "end"
 )
 
