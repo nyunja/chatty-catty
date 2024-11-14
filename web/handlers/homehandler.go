fb9be1a72f923890086b36618bc13134c68a9e11
johnotieno0 <poptiff@gmail.com>
2024-11-14 10:03:30 +0300

    feat: implement homehandler to serve chat html template

---
+++ web/handlers/homehandler.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	tmpl, err := template.ParseFiles("../../templates/index.html")
+	if err != nil {
+		fmt.Println("index.html not found")
+		http.NotFound(w, r)
+		return
+	}
+	tmpl.Execute(w, "hello")
+}
