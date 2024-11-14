bd4edfcfd3d6d20cf6374bd222c81b5add30ffb6
johnotieno0 <poptiff@gmail.com>
2024-11-14 09:55:11 +0300

    update: handlers package import and handle the /chat endpoint

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	h "chatty/handlers"
+	h "chatty/web/handlers"
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Starting server on port 8000...")
 	staticDir := "./static/"
 	staticURL := "/static/"
 	http.Handle(staticURL, h.CustomStaticServer(staticDir))
 	http.HandleFunc("/", h.HomeHandler)
+	http.HandleFunc("/chat", h.ChatHandler)
 	http.ListenAndServe(":8000", nil)
 }
